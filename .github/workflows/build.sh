ebe4d3c1bd5bc0ff22fa0f80bdb81cd059e83035
Giles Knap <giles.knap@diamond.ac.uk>
2023-11-07 21:04:24 +0000

    trying no CI cache

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -10,31 +10,22 @@
 #   PUSH: if true, push the container image to the registry
 #   TAG: the tag to use for the container image
 #   PLATFORM: the platform to build for (linux/amd64 or linux/arm64)
-#   CACHE: the directory to use for caching
 
 export EC_TAG="--tag ${TAG:-latest}"
 export EC_PLATFORM="--platform ${PLATFORM:-linux/amd64}"
-export EC_CACHE="${CACHE:-/tmp/ec-cache}"
 if [[ "${PUSH}" == 'true' ]] ; then EC_PUSH='--push' ; fi
 
 THIS=$(dirname ${0})
 set -xe
-mkdir -p ${CACHE}
 
 # get the current version of ec CLI
 pip install -r ${THIS}/../../requirements.txt
-
-# add cache arguments - local file cache passed by github seems to be most reliable
-export EC_CARGS="
-    --cache-from type=local,src=${EC_CACHE}
-    --cache-to type=local,dest=${EC_CACHE}
-"
 
 # add extra cross compilation platforms below if needed  e.g.
 #   ec dev build  --arch rtems ... for RTEMS cross compile
 
 # build runtime and developer images
-ec -v dev build ${EC_TAG} ${EC_PLATFORM} ${EC_PUSH} ${EC_CARGS}
+ec -v dev build ${EC_TAG} ${EC_PLATFORM} ${EC_PUSH}
 
 # extract the ioc schema from the runtime image
 ec dev launch-local ${EC_TAG} --execute \
